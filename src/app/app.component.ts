bc02da00e2476e9f73d8652145a154956dbf7ba8
Mikhail Sarnov <kpblm2@gmail.com>
2024-01-18 17:43:48 +0100

    Beginning our angular todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { NgFor } from '@angular/common';
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  imports: [
+    NgFor
+  ]
 })
 export class AppComponent {
-  title = 'todo';
+  title = "todo";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  
 }
